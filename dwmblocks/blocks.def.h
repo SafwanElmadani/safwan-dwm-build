bd2fb4385497996e46f6ebb11e668406ec157b01
safwanelmadani <safwanelmadani@email.arizona.edu>
2021-04-02 22:56:38 -0700

    added cpu and ssd to blocks

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,17 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {" 🐧 ", "/home/safwan/.config/scripts/dtscripts/kernel",		    360,		        2},
+    {" 🐧 ", "/home/safwan/.config/scripts/dtscripts/kernel",		    3600,		        2},
 
-	{" 🔺 ", "/home/safwan/.config/scripts/dtscripts/upt",		        60,		            2},
+	{"", 	"/home/safwan/.config/scripts/dwmdisk.sh",		        3600,		            0},
 
-	{" 📦 ", "/home/safwan/.config/scripts/dtscripts/pacupdate",		360,		        9},
+	{" 📦 ", "/home/safwan/.config/scripts/dtscripts/pacupdate",		3600,		        9},
 	
 	{" 💻 ", "/home/safwan/.config/scripts/dtscripts/memory",	        6,		            1},
 
 	{" 🔊 ", "/home/safwan/.config/scripts/dtscripts/volume",			2,		            10},
 
+	{"",  	 "/home/safwan/.config/scripts/dwmcpu.sh",		 			2,		            0},
+
 	{" 🕑 ", "/home/safwan/.config/scripts/dtscripts/clock",			5,		            0},
+
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
