275ba0005725b951abd5d20be5c82ac586bd1e99
safwanelmadani <safwanelmadani@email.arizona.edu>
2021-06-15 18:48:51 -0700

    add weather widget

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "/home/safwan/.config/scripts/weather",		    1800,		        3},
+
     {"", "/home/safwan/.config/scripts/dtscripts/kernel",		    3600,		        2},
 
 	{"", 	"/home/safwan/.config/scripts/dwmdisk",		   		        3600,		        0},
