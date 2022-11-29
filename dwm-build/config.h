19cb60adf3de841817a19b8038691a087df12f0d
safwanelmadani <safwanelmadani@arizona.edu>
2022-11-28 18:07:20 -0700

    Mon Nov 28 06:07:20 PM MST 2022

--- dwm-build/config.h
+++ dwm-build/config.h
@@ -15,9 +15,9 @@ static const unsigned int gappov    = 15;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki:size=13:antialias=true:autohint=true",
-										"Twemoji:size=11:antialias=true:autohint=true" };
-static const char dmenufont[]       = "mononoki:size=13";
+static const char *fonts[]          = { "JetBrains Mono:size=13:antialias=true:autohint=true",
+										"Twemoji:size=11:antialias=true:autohint=true"};
+static const char dmenufont[]       = "JetBrains Mono:size=13";
 static const char col_gray1[]       = "#23272F"; //"#222222"; //background color
 static const char col_gray2[]       = "#444444"; //inactive window border color
 static const char col_gray3[]       = "#FFFFFF"; //"#bbbbbb"; // font color
